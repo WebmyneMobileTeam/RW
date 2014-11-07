71edbf10242a86fc08f128c6c4ef220ed98e489e
Nirav Kalola <niravkalola1991@gmail.com>
2014-11-07 19:30:01 +0530

    xyz

--- app/src/main/java/com/webmyne/rightway/Model/AppConstants.java
+++ app/src/main/java/com/webmyne/rightway/Model/AppConstants.java
@@ -5,10 +5,10 @@ package com.webmyne.rightway.Model;
  */
 public class AppConstants {
 
-    public static String customerRegistration="http://ws-srv-net.in.webmyne.com/Applications/Android/RiteWayServices/Customer.svc";
-    public static String getActiveDrivers="http://ws-srv-net.in.webmyne.com/Applications/Android/RiteWayServices/Customer.svc/json/ActiveDrivers";
-    public static String imageUploadPath="";
-    // username: riteway
-    // password: riteway
+    public static final String customerRegistration="http://ws-srv-net.in.webmyne.com/Applications/Android/RiteWayServices/Customer.svc";
+    public static final String getActiveDrivers="http://ws-srv-net.in.webmyne.com/Applications/Android/RiteWayServices/Customer.svc/json/ActiveDrivers";
+    public static String imageUploadPath="D:\\FTPFolder\\Riteway\\Riteway\\Images";
+    // ftp username: riteway
+    // ftp password: riteway
 
 }
